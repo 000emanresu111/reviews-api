0773bb8a1db2f976b0fe253b83d61a80bfba1bff
Andrea Evangelista <andrea.evangelista@techedgegroup.com>
2023-08-30 10:09:16 +0200

    refactor(scraper): separate service from utils

--- app/scraper/scraping_service.py
+++ app/scraper/scraping_service.py
@@ -1,54 +1,18 @@
+from shutil import which
+from time import sleep
 from typing import List
+
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from app.models.schemas import (
-    RestaurantInfo,
-    RestaurantReview,
-    ReviewInfo,
-    ReviewSentiment,
-)
-from shutil import which
-from time import sleep
+
+from app.models.schemas import RestaurantInfo, RestaurantReview
+from app.scraper.scraping_utils import (get_normalized_rating,
+                                        scrape_review_element)
 
 WEBDRIVER_PATH = which("chromedriver")
-
-
-def get_normalized_rating(percentage: float) -> float:
-    return round((percentage / 100) * 5, 1)
-
-
-def get_text_element(review, selector):
-    try:
-        element = review.find_element(By.CSS_SELECTOR, selector)
-        return element.text
-    except NoSuchElementException:
-        return None
-
-
-def scrape_review_element(review_element):
-    rating_percentage_element = review_element.find_element(
-        By.CSS_SELECTOR, ".RatingMultiStarVariant_c-rating-mask_1c0Q3"
-    )
-    rating_percentage_style = rating_percentage_element.get_attribute("style")
-    percentage = float(rating_percentage_style.split(":")[-1].strip().replace("%;", ""))
-    normalized_rating = get_normalized_rating(percentage)
-
-    review_text = get_text_element(review_element, '[data-test-id="review-text"]')
-    reviewer = get_text_element(review_element, '[data-test-id="review-author"]')
-    review_date = get_text_element(review_element, '[data-test-id="review-date"]')
-
-    review_info = ReviewInfo(
-        review_date=review_date,
-        review_reviewer=reviewer,
-        review_text=review_text,
-        review_sentiment=ReviewSentiment.NONE,
-        review_rating=normalized_rating,
-    )
-
-    return review_info
 
 
 def click_show_more(driver):
---
+++ app/scraper/scraping_utils.py
@@ -0,0 +1,38 @@
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from app.models.schemas import ReviewInfo, ReviewSentiment
+
+
+def get_normalized_rating(percentage: float) -> float:
+    return round((percentage / 100) * 5, 1)
+
+
+def get_text_element(element, selector) -> str:
+    try:
+        element = element.find_element(By.CSS_SELECTOR, selector)
+        return element.text
+    except NoSuchElementException:
+        return None
+
+
+def scrape_review_element(review_element):
+    rating_percentage_element = review_element.find_element(
+        By.CSS_SELECTOR, ".RatingMultiStarVariant_c-rating-mask_1c0Q3"
+    )
+    rating_percentage_style = rating_percentage_element.get_attribute("style")
+    percentage = float(rating_percentage_style.split(":")[-1].strip().replace("%;", ""))
+    normalized_rating = get_normalized_rating(percentage)
+
+    review_text = get_text_element(review_element, '[data-test-id="review-text"]')
+    reviewer = get_text_element(review_element, '[data-test-id="review-author"]')
+    review_date = get_text_element(review_element, '[data-test-id="review-date"]')
+
+    review_info = ReviewInfo(
+        review_date=review_date,
+        review_reviewer=reviewer,
+        review_text=review_text,
+        review_sentiment=ReviewSentiment.NONE,
+        review_rating=normalized_rating,
+    )
+
+    return review_info
